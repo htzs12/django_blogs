e393069304b10792f4200919f5774c6db9b3f9e9
htzs12 <www.htzs@qq.com>
2018-05-11 07:10:34 +0800

    first

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import BlogArticles
+
+def blog_title(request):
+    blogs=BlogArticles.objects.all()
+    return render(request,'blog/title.html',{'blogs':blogs})
+
